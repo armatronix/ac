ca64505da34c141ec91b65db02b988d9f627caaf
Hans Lindauer <armatronix@gmail.com>
2018-08-31 23:39:36 -0400

    remove libraries from github

--- libraries/Infrared/src/IrSenderSimulator.cpp
+++
@@ -1,9 +0,0 @@
-#include "IrSenderSimulator.h"
-
-void IrSenderSimulator::send(const IrSequence& irSequence, frequency_t frequency) {
-    stream.print(F("IrSenderSimulator: "));
-    bool printedSomething = IrSignal::dumpFrequency(stream, frequency);
-    if (printedSomething)
-        stream.print(' ');
-    irSequence.dump(stream, true);
-}
