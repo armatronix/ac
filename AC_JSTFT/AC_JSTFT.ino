bfe8c613ef800632bb8e77d78d59d58509b4864f
Hans Lindauer <armatronix@gmail.com>
2018-08-31 23:43:15 -0400

    Another test

--- AC_JSTFT/AC_JSTFT.ino
+++ AC_JSTFT/AC_JSTFT.ino
@@ -4,7 +4,7 @@
  ****************************************************/
 
 #include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735..
 #include <SPI.h>
 #include <Arduino.h>
 #include <Wire.h>
